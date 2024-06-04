f2aa80fc0efbfb6435bcd92e492f09e08e2ca3a9
Your Namesh <sh3287906@gmail.com>
2024-06-05 01:46:39 +0300

    פיצול

---
+++ src/components/employee/addEmployee.js
@@ -0,0 +1,212 @@
+import React, { useEffect } from 'react';
+import { FormControl, Input, InputLabel, MenuItem, Select, Checkbox, IconButton, InputAdornment, Button } from "@material-ui/core";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useFieldArray, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import { addNewEmployee, editEmployee, getEmployees } from '../../service/EmployeeService';
+import moment from 'moment';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import SendIcon from '@mui/icons-material/Send';
+
+
+const schema = yup.object({
+    firstName: yup.string().required("Required field").min(3, "at list 3 characters"),
+    lastName: yup.string().required("Required field").min(3, "at list 3 characters"),
+    tz: yup.string().required("Required field").max(9, "Maximum 9 characters"),
+    startDate: yup.date().typeError('The date is invalid').required("Required field"),
+    birthDate: yup.date().typeError('The date is invalid').required("Required field"),
+    gender: yup.number().required("Required field"),
+    isActive: yup.bool().default(true),
+    roles: yup.array().of(
+        yup.object().shape({
+            roleId: yup.number().required("Required field"),
+            isManagement: yup.bool().required("Required field"),
+            startDateRole: yup.date().typeError('The date is invalid').required("Required field"),
+        })
+    ),
+}).required();
+
+const AddEmployee = () => {
+    const { state } = useLocation();
+    const employee = state?.employee;
+    const dispatch = useDispatch();
+    const roles = useSelector(state => state.roles);
+    const navig = useNavigate();
+
+    const { register, handleSubmit, formState: { errors }, control, setValue } = useForm({
+        resolver: yupResolver(schema),
+        defaultValues: employee,
+    });
+
+    useEffect(() => {
+        dispatch(getEmployees());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (employee) {
+            Object.keys(employee).forEach(key => {
+                if (key === "startDate" || key === "birthDate") {
+                    setValue(key, moment(employee[key]).format('YYYY-MM-DD'));
+                } else if (key === "roles") {
+                    employee.roles.forEach((role, index) => {
+                        setValue(`roles[${index}].startDateRole`, moment(role.startDateRole).format('YYYY-MM-DD'));
+                    });
+                } else {
+                    setValue(key, employee[key]);
+                }
+            });
+        }
+    }, [employee, setValue]);
+
+    const isRoleIdExists = (index) => {
+        const roleIdsArray = fieldsRoles.map(field => field.roleId);
+        var isExist = false
+        roleIdsArray?.forEach((role) => {
+            if (role == index)
+                isExist = true;
+        })
+        return isExist;
+    }
+
+    const onSubmit = (data) => {
+        if (state) {
+            data["id"] = employee.id;
+            dispatch(editEmployee(data.id, data, navig));
+        } else {
+
+            dispatch(addNewEmployee(data, navig));
+        }
+    };
+
+    const { fields: fieldsRoles, append: appendRole, remove: removeRole } = useFieldArray({
+        control,
+        name: "roles",
+    });
+
+    return (
+        <div className='add background-img backgroundPage'>
+            <div>
+                {state ? <h2>Edit Employee</h2> : <h2>Add Employee</h2>}
+            </div>
+            <form className='my-form' onSubmit={handleSubmit(onSubmit)}>
+
+
+
+                {/* <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
+                <FormControl variant="standard" style={{ width: '250px' }}>
+
+                    <InputLabel id="demo-simple-input-standard-label"> FirstName </InputLabel>
+                    <Input {...register("firstName")} />
+                    <p>{errors.firstName?.message}</p>
+                </FormControl>
+                <br />
+
+                <FormControl variant="standard" style={{ width: '250px' }}>
+                    <InputLabel id="demo-simple-input-standard-label"> LastName </InputLabel>
+                    <Input {...register("lastName")} />
+                    <p>{errors.lastName?.message}</p>
+                </FormControl>
+                <br />
+
+                <FormControl variant="standard" style={{ width: '250px' }}>
+                    <InputLabel id="demo-simple-input-standard-label"> Tz </InputLabel>
+                    <Input {...register("tz")} />
+                    <p>{errors.tz?.message}</p>
+                </FormControl>
+                <br />
+
+                <FormControl variant="standard" style={{ width: '250px' }}>
+                   <p> StartDate </p>
+                    <Input {...register("startDate")} type="date" />
+                    <p>{errors.startDate?.message}</p>
+                </FormControl>
+                <br />
+
+                <FormControl variant="standard" style={{ width: '250px' }}>
+                    <p> BirthDate </p>
+                    <Input {...register("birthDate")} type="date" />
+                    <p>{errors.birthDate?.message}</p>
+                </FormControl>
+                <br />
+
+                <FormControl variant="standard" style={{ width: '250px' }}>
+                    <InputLabel id="demo-simple-select-standard-label-gender">Gender</InputLabel>
+                    <Select
+                        {...register("gender")}
+                        labelId="demo-simple-select-standard-label-gender"
+                        id="demo-simple-select-standard-gender"
+                        label="Gender"
+                        defaultValue={employee?.gender || ""}
+                    >
+                        <MenuItem value={1}>Male</MenuItem>
+                        <MenuItem value={2}>Female</MenuItem>
+                        <MenuItem value={3}>Other</MenuItem>
+                    </Select>
+                </FormControl>
+                <br />
+
+                {fieldsRoles.map((field, index) => (
+                    <div className='roles' key={field.id}>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    {...register(`roles.${index}.isManagement`)}
+                                    defaultChecked={field.isManagement}
+                                    color="black"
+                                />
+                            }
+                            label="is Manager?"
+                            labelPlacement="end"
+                        />
+
+                        <FormControl className='my-space' variant="standard" sx={{ m: 1, minWidth: 120, maxWidth: 185 }} style={{ marginLeft: '20px' }}>
+                            <InputLabel id={`demo-simple-input-standard-label-roleId-${index}`}>Choose a role  </InputLabel>
+                            <Select
+                                {...register(`roles.${index}.roleId`)}
+                                labelId={`demo-simple-select-standard-label-roleId-${index}`}
+                                id={`demo-simple-select-standard-roleId-${index}`}
+                                label="Choose a role"
+                                defaultValue={field.roleId || ""}
+                            >
+
+                                {roles?.filter(role => !isRoleIdExists(role.id) || role.id === field.roleId)
+                                    .map((role) => (
+                                        <MenuItem key={role.id} value={role.id} >
+                                            {role.name}
+                                        </MenuItem>
+                                    ))}
+                            </Select>
+                        </FormControl>
+
+                        <FormControl className='my-space' variant="standard" sx={{ m: 1, minWidth: 120, maxWidth: 185 }} style={{ marginLeft: '20px' }}>
+                            <p> Role's Start Date</p>
+                            <Input {...register(`roles.${index}.startDateRole`)} type="date" />
+                            <p>{errors?.roles?.[index]?.startDateRole?.message}</p>
+                        </FormControl>
+
+
+                        <IconButton aria-label="delete" onClick={() => removeRole(index)} style={{ marginLeft: '20px' }}>
+                            <DeleteForeverIcon />
+                        </IconButton>
+
+                    </div>
+                ))
+                }
+                <Button className='Download-btn' variant="outlined" startIcon={<AddCircleIcon />} onClick={() => appendRole({})} style={{ color: '#00aa9d', marginTop: '10px' }}>
+                    Add Role
+                </Button>
+                <br />
+                <Button type="submit" variant="outlined" endIcon={<SendIcon />} style={{ color: '#00aa9d', marginTop: '10px' }}>
+                    Submit
+                </Button>
+            </form >
+        </div >
+    );
+};
+
+export default AddEmployee;
+
---
+++ src/components/employee/displayEmployees.js
@@ -0,0 +1,163 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, IconButton, TextField } from '@material-ui/core';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import EditIcon from '@mui/icons-material/Edit';
+import { deleteEmployee } from '../../service/EmployeeService';
+import { useNavigate } from 'react-router-dom';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import * as XLSX from 'xlsx';
+import EmployeeDetailsPopup from './employeeDedailsPopup';
+import DownloadIcon from '@mui/icons-material/Download';
+
+const CustomTableCell = withStyles(theme => ({
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const styles = theme => ({
+    root: {
+        width: '80%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginBottom: '15px'
+    },
+    table: {
+        minWidth: 700,
+        padding: '80px',
+
+    },
+    employee: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.background.default,
+        },
+    },
+    searchField: {
+        marginTop: theme.spacing.unit * 2,
+        marginBottom: theme.spacing.unit * 2,
+        marginLeft: theme.spacing.unit,
+        width: '200px',
+        border: '2px solid #00aa9d',
+        borderRadius: '8.5%'
+
+    },
+});
+
+const DisplayEmployees = (props) => {
+    const { classes } = props;
+    const employees = useSelector(state => state.employees);
+    const dispatch = useDispatch();
+    const navig = useNavigate();
+    const [searchText, setSearchText] = useState('');
+    const [popupEmployee, setPopupEmployee] = useState(null);
+    const [open, setOpen] = useState(false);
+
+    const filteredEmployees = employees.filter(employee =>
+        employee.firstName.toLowerCase().includes(searchText.toLowerCase()) ||
+        employee.lastName.toLowerCase().includes(searchText.toLowerCase()) ||
+        employee.tz.toLowerCase().includes(searchText.toLowerCase()) ||
+        employee.startDate.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    const exportToExcel = () => {
+        const wb = XLSX.utils.book_new();
+        const ws = XLSX.utils.json_to_sheet(filteredEmployees);
+        XLSX.utils.book_append_sheet(wb, ws, 'Employees');
+        XLSX.writeFile(wb, 'employees.xlsx');
+    };
+
+    const handleSearchChange = (event) => {
+        setSearchText(event.target.value);
+    };
+
+    const handleRowClick = (employee) => {
+        if (!open) {
+            setPopupEmployee(employee);
+            setOpen(true);
+        }
+    };
+
+
+
+    useEffect(() => {
+        if (open) {
+            setPopupEmployee(popupEmployee);
+        }
+    }, [open, popupEmployee])
+
+
+    return (<>
+        <TextField
+            className={classes.searchField}
+            label="search box"
+            value={searchText}
+            onChange={handleSearchChange}
+        />
+        <Paper className={classes.root}>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
+                        <CustomTableCell align="center">First Name</CustomTableCell>
+                        <CustomTableCell align="center">Last Name</CustomTableCell>
+                        <CustomTableCell align="center">Tz</CustomTableCell>
+                        <CustomTableCell align="center">Start Date</CustomTableCell>
+                        <CustomTableCell align="center">Delete</CustomTableCell>
+                        <CustomTableCell align="center">Edit</CustomTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {filteredEmployees.map(employee => (
+                        <TableRow
+                            className={classes.employee}
+                            key={employee.id}
+                            onClick={() => handleRowClick(employee)}
+                        >
+                            <CustomTableCell align="center">{employee.firstName}</CustomTableCell>
+                            <CustomTableCell align="center">{employee.lastName}</CustomTableCell>
+                            <CustomTableCell align="center">{employee.tz}</CustomTableCell>
+                            <CustomTableCell align="center">{employee.startDate}</CustomTableCell>
+                            <CustomTableCell align="center">
+                                <IconButton onClick={() => dispatch(deleteEmployee(employee.id, navig))}>
+                                    <DeleteForeverIcon />
+                                </IconButton>
+                            </CustomTableCell>
+                            <CustomTableCell align="center">
+                                <IconButton onClick={() => navig("/editEmployee", { state: { employee } })}>
+                                    <EditIcon />
+                                </IconButton>
+                            </CustomTableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+                <IconButton onClick={() => navig("/addEmployee")}>
+                    <AddCircleIcon />
+                </IconButton>
+            </Table>
+        </Paper>
+        <EmployeeDetailsPopup open={open} employee={popupEmployee} onClose={() => setOpen(false)} />
+        <Button className='Download-btn' variant="outlined" startIcon={<DownloadIcon />} onClick={exportToExcel} style={{ color: '#00aa9d' }}>
+            Export to Excel
+        </Button>
+    </>);
+}
+
+DisplayEmployees.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(DisplayEmployees);
